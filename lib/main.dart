e4a12ea9dcd8ac3ec2fcf012b4106bf72b3f3df4
Jaimul Islam Akik <105303801+devjaimul@users.noreply.github.com>
2024-03-27 18:02:04 +0600

    updated

--- lib/main.dart
+++ lib/main.dart
@@ -12,9 +12,9 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       // debugShowCheckedModeBanner: false,
       home: Scaffold(
-        body:Column(
+        backgroundColor: Colors.grey.shade200,
+        body: Column(
           mainAxisAlignment: MainAxisAlignment.center,
-
           children: [
             Text(
               'hello world',
@@ -25,22 +25,70 @@ class MyApp extends StatelessWidget {
                   color: Colors.teal,
                   decoration: TextDecoration.underline),
             ),
-            Row(children: [
-              Text(
-                'hello world',
-                textAlign: TextAlign.center,
-                style: TextStyle(
-                    fontSize: 20,
-                    fontWeight: FontWeight.w400,
-                    color: Colors.teal,
-                    decoration: TextDecoration.underline),
-              ),
-            ],)
+            Row(
+              children: [
+                Text(
+                  'hello world',
+                  textAlign: TextAlign.end,
+                  style: TextStyle(
+                      fontSize: 20,
+                      fontWeight: FontWeight.w400,
+                      color: Colors.teal,
+                      decoration: TextDecoration.underline),
+                ),
+                Icon(
+                  Icons.home,
+                  color: Colors.blueAccent,
+                )
+              ],
+            )
           ],
         ),
+        appBar: AppBar(
+          title: Center(
+              child: Text(
+            'app bar',
+            style: TextStyle(color: Colors.white),
+          )),
+          backgroundColor: Colors.green,
+          leading:IconButton( onPressed: ()
+          {
+            ScaffoldMessenger.of(context).showSnackBar(
+              SnackBar(
+                content: Text('what did you wnat to add'),
+                // backgroundColor: Colors.cyan,
+                duration: Duration(seconds: 2),
+              ),
+            );
+          },
 
-        appBar: AppBar(title: Text('app bar'),),
 
+          icon: Icon(Icons.menu),
+          ),
+          actions: [
+            IconButton(
+                onPressed: ()
+                {
+                  ScaffoldMessenger.of(context).showSnackBar(
+                    SnackBar(
+                      content: Text('what did you wnat to add'),
+                     // backgroundColor: Colors.cyan,
+                      duration: Duration(seconds: 2),
+                    ),
+                  );
+                },
+                icon: Icon(
+                  Icons.add,
+                  color: Colors.white,
+                )),
+            IconButton(
+                onPressed: () {},
+                icon: Icon(
+                  Icons.h_plus_mobiledata,
+                  color: Colors.white,
+                )),
+          ],
+        ),
       ),
     );
   }
